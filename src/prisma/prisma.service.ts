64f09724d96b0a3b6b2d13acccbd5c73861d0926
mai <fujisawamai21@gmail.com>
2023-06-19 12:14:47 +0900

    prisma seviceを必要なモジュール（auth,todo, user）から使えるようにする設定

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
-
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client';
 @Injectable()
-export class PrismaService {}
+// PrismaサービスでPrismaClientの機能を使用するため、PrismaClientを継承した
+// PrismaServiceクラスを作成する
+export class PrismaService extends PrismaClient {
+  // Prismaサービスの中でConfigServiceを使用する
+  constructor(private readonly config: ConfigService) {
+    // 継承したクラスはsupuer()を使用して親クラスのコンストラクタを呼び出す必要がある
+    super({
+      datasources: {
+        db: {
+          url: config.get('DATABASE_URL'),
+        },
+      },
+    });
+  }
+}
